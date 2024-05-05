0d24f411009544c72c3657610c3d2ba84f17ec48
noorharidy19 <noor2206987@miuegypt.edu.eg>
2024-05-06 01:18:33 +0300

    Cart

--- Prod.js
+++ Prod.js
@@ -1,12 +1,14 @@
 function showPopup(containerIds) {
     containerIds.forEach(function(id) {
         document.getElementById(id).style.display = "block";
+        document.getElementById('overlay').style.display = 'block';
     });
 }
 
 function hidePopup(containerIds) {
     containerIds.forEach(function(id) {
         document.getElementById(id).style.display = "none";
+        document.getElementById('overlay').style.display = 'none';
     });
 }
 function showP(containerIds) {
@@ -349,3 +351,39 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+let initialPrices = {};
+
+function changeQuantity(change, quantityId, priceId) {
+    let quantityElement = document.getElementById(quantityId);
+    let quantity = parseInt(quantityElement.textContent);
+    
+    quantity += change;
+    if (quantity < 1) {
+      quantity = 1;
+    } else if (quantity > 5) {
+      quantity = 5;
+    }
+    
+    let priceElement = document.getElementById(priceId);
+    if (!initialPrices[priceId]) {
+      initialPrices[priceId] = parseFloat(priceElement.textContent);
+    }
+    
+    let pricePerItem = initialPrices[priceId];
+    
+    quantityElement.textContent = quantity;
+    priceElement.textContent = (pricePerItem * quantity).toFixed(2);
+    getTotalPrice();
+  }
+function getTotalPrice() {
+    let priceIds = ['price1', 'price2', 'price3']; 
+    let totalPrice = 0;
+    priceIds.forEach(function(priceId) {
+      let priceElement = document.getElementById(priceId);
+      totalPrice += parseFloat(priceElement.textContent);
+    });
+    document.getElementById('total').textContent = totalPrice.toFixed(2);
+  }
+  window.onload = function() {
+    getTotalPrice();
+  };
